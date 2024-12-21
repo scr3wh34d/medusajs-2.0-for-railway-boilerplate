dda4f54c420ebc3d037f99499c80f22030464ceb
scr3wh34d <scr3wh34d@gmail.com>
2024-12-21 16:22:21 -0600

    git commit -m "widgets removed from .gitignore - I think I was looking at the wrong docs"

--- backend/src/admin/widgets/product-widget.tsx
+++ backend/src/admin/widgets/product-widget.tsx
@@ -1,17 +1,20 @@
-import { defineWidgetConfig } from "@medusajs/admin-shared"
+import { defineWidgetConfig } from "@medusajs/admin-sdk"
+import { Container, Heading } from "@medusajs/ui"
 
 // The widget
 const ProductWidget = () => {
     return (
-        <div>
-            <h2>Product Widget</h2>
-        </div>
+        <Container className="divide-y p-0">
+            <div className="flex items-center justify-between px-6 py-4">
+                <Heading level="h2">Product Widget</Heading>
+            </div>
+        </Container>
     )
 }
 
 // The widget's configurations
 export const config = defineWidgetConfig({
-    zone: "product.details.after",
+    zone: "product.details.before",
 })
 
 export default ProductWidget
