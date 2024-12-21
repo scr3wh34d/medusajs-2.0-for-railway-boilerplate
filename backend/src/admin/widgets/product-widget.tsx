99d3ba8f33d1d7a53d3a3231217628e7ec7036af
scr3wh34d <scr3wh34d@gmail.com>
2024-12-21 16:38:46 -0600

    git commit -m "widgets removed from .gitignore - I think I was looking at the wrong docs -- I believe I also need to remove the repo I was trying to add as that was part of the wrong docs maybe now?"

--- backend/src/admin/widgets/product-widget.tsx
+++ backend/src/admin/widgets/product-widget.tsx
@@ -1,12 +1,20 @@
 import { defineWidgetConfig } from "@medusajs/admin-sdk"
 import { Container, Heading } from "@medusajs/ui"
+import {
+    DetailWidgetProps,
+    AdminProduct,
+} from "@medusajs/framework/types"
 
 // The widget
-const ProductWidget = () => {
+const ProductWidget = ({
+                           data,
+                       }: DetailWidgetProps<AdminProduct>) => {
     return (
         <Container className="divide-y p-0">
             <div className="flex items-center justify-between px-6 py-4">
-                <Heading level="h2">Product Widget</Heading>
+                <Heading level="h2">
+                    Product Widget {data.title}
+                </Heading>
             </div>
         </Container>
     )
